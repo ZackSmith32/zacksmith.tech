d67b95e156a6cddb9c32b4ccdd15df777a3f405c
Zack Smith <z.smith32@gmail.com>
2017-01-10 14:44:33 -0800

    back end up and running legooooo

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var _42 = require('./routes/42');
+var side = require('./routes/side');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/42', _42);
+app.use('/side', side);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
